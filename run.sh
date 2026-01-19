e84334b9d8471260944a4684d1075146b3b54cea
Andrew <sl4shed@proton.me>
2026-01-19 23:12:08 +0200

    nah

--- run.sh
+++ run.sh
@@ -1,7 +1,18 @@
-cd build-psp
-psp-cmake  ..
-make
-read -p "run emulated? [y/n]: " answer
-if [ "$answer" == "y" ]; then
-    PPSSPPSDL /home/andrew/Projects/calamity-engine/build
+read -p "psp build? [y/n]: " psp
+if [ "$psp" == "y" ]; then
+    cd build-psp
+    psp-cmake -DBUILD_TARGET=PSP ..
+    make
+    read -p "run? [y/n]: " answer
+    if [ "$answer" == "y" ]; then
+        PPSSPPSDL /home/andrew/Projects/calamity-engine/build
+    fi
+else
+    cd build
+    cmake -DBUILD_TARGET=NATIVE ..
+    make
+    read -p "run? [y/n]: " answer
+    if [ "$answer" == "y" ]; then
+        ./calamity-engine
+    fi
 fi
