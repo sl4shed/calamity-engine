24c95befd342859a23e32043d0b16e9438dc4245
Andrew <sl4shed@proton.me>
2026-03-15 22:01:50 +0200

    to feel this moment

--- backend/services/input/input.cpp
+++ backend/services/input/input.cpp
@@ -5,6 +5,8 @@
 #include "keycode.hpp"
 #include <iostream>
 #include <SDL3/SDL.h>
+#include <vector>
+#include <algorithm>
 
 void Input::update(float deltaTime)
 {
@@ -12,20 +14,24 @@ void Input::update(float deltaTime)
     inputs.clear();
 
     SDL_Event event;
-    while (SDL_PollEvent(&event))
+    while (SDL_PollEvent(&event)) {}
+
+    const bool* state = SDL_GetKeyboardState(nullptr);
+
+    for (int i = 0; i < SDL_SCANCODE_COUNT; i++)
     {
-        if (event.type == SDL_EVENT_KEY_DOWN)
-        {
-            Keycode keycode = (Keycode)event.key.key;
-            Logger::debug("{}", (uint)keycode);
-            inputs.emplace_back(keycode);
-        }
+        bool curr = state[i];
+        bool prev = std::any_of(prevInputs.begin(), prevInputs.end(),
+            [i](InputEvent& e) { return e.isKeycode((Keycode)i); });
+
+        if (curr && !prev)
+            inputs.emplace_back((Keycode)i, InputAction::KeyPressed);
+        else if (!curr && prev)
+            inputs.emplace_back((Keycode)i, InputAction::KeyReleased);
     }
 
-    for (const InputEvent& e : inputs)
-    {
+    for (InputEvent& e : inputs)
         Services::engine()->root.input(e);
-    }
 }
 
 bool InputEvent::isKeycode(Keycode compare) {
@@ -34,6 +40,7 @@ bool InputEvent::isKeycode(Keycode compare) {
     return condition;
 }
 
-InputEvent::InputEvent(Keycode k) {
+InputEvent::InputEvent(Keycode k, InputAction ac) {
     this->keycode = k;
+    this->action = ac;
 }
--- backend/services/input/input.hpp
+++ backend/services/input/input.hpp
@@ -1,18 +1,29 @@
 #pragma once
-
+#include <SDL3/SDL.h>
 #include "../../core/definitions.hpp"
 #include "keycode.hpp"
+
+enum class InputAction {
+    KeyPressed = SDL_EVENT_KEY_DOWN,
+    KeyReleased = SDL_EVENT_KEY_UP,
+    MouseButtonPressed = SDL_EVENT_MOUSE_BUTTON_DOWN,
+    MouseButtonReleased = SDL_EVENT_MOUSE_BUTTON_UP,
+    GamepadButtonPressed = SDL_EVENT_GAMEPAD_BUTTON_DOWN,
+    GamepadButtonReleased = SDL_EVENT_GAMEPAD_BUTTON_UP,
+};
 
 // multiple per frame
 class InputEvent
 {
 public:
-    InputEvent(Keycode keycode);
+    InputEvent(Keycode keycode, InputAction action);
 
     bool isKeycode(Keycode compare);
 private:
     Keycode keycode;
+    InputAction action;
 };
+
 
 #include "../../core/node/components.hpp"
 
@@ -30,3 +41,4 @@ private:
     std::vector<InputEvent> inputs;
     std::vector<InputEvent> prevInputs;
 };
+
